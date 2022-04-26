33475a58d7774718b952d4e90d8580ccd25e01af
Harshiikaa <93309678+Harshiikaa@users.noreply.github.com>
2022-04-26 11:49:42 +0545

    task 1 vanda agaadi ko tasks

---
+++ Practice.java
@@ -0,0 +1,11 @@
+public class Practice {
+    public static void main(String[] args){
+        int a = 2, b=3, e;
+        String c = "Ram", d="Sita", f;
+        e=a;
+        f=d;
+        System.out.println(b>e);
+        System.out.println(f!=c);
+
+    }
+}
